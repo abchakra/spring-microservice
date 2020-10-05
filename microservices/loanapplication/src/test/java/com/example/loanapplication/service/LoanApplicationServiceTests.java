4357fd65d1be4a49dbd9dd3a9d86c152a5c6e975
abhishek chakraborty <abhishek.chakraborty26@gmail.com>
2020-10-05 13:59:00 +0200

    Add docker support and docker-compose to build the container landscape

---
+++ microservices/loanapplication/src/test/java/com/example/loanapplication/service/LoanApplicationServiceTests.java
@@ -0,0 +1,64 @@
+package com.example.loanapplication.service;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import com.example.loanapplication.model.LoanApplication;
+import com.example.loanapplication.model.LoanApplicationsDto;
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.WireMock;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+
+@SpringBootTest
+@TestPropertySource(value = "classpath:test.properties")
+public class LoanApplicationServiceTests {
+	@Autowired
+	private LoanApplicationService loanApplicationService;
+
+	private WireMockServer wireMockServer;
+
+	@BeforeEach
+	public void setupWireMockServer() {
+		// initialize WireMock server
+		wireMockServer = new WireMockServer(8081);
+		wireMockServer.start();
+
+		// configure response stub
+		wireMockServer.stubFor(
+			WireMock.get("/api/customers/11")
+				.willReturn(aResponse()
+				.withHeader("Content-Type", "application/json")
+				.withStatus(200)
+				.withBodyFile("json/customers-response.json"))
+			);
+	}
+
+	@AfterEach
+	public void stopWireMockServer() {
+		wireMockServer.stop();
+	}
+
+	@Test
+	public void testGetLoanApplicationSuccessfully() {
+		LoanApplicationsDto loanApplicationsDto = loanApplicationService.findCustomerById(11);
+
+		Assertions.assertNotNull(loanApplicationsDto, "LoanApplications should exist");
+	}
+
+	@Test
+	public void testCreateNewLoanApplicationSuccessful() {
+
+		LoanApplication newLoanApplication = new LoanApplication(11, 1000, 12);
+
+		LoanApplication loanApplication = loanApplicationService.save(newLoanApplication);
+
+		Assertions.assertEquals(loanApplication.getCustomerId(), newLoanApplication.getCustomerId(),
+				"Customer ID should be 11");
+	}
+}
